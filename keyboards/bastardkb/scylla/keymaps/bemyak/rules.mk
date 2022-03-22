410e7cbad5db5b3b326b0b2605452f73e72db16b
­ <bemyak@pm.me>
2022-03-22 15:10:49 +0200

    Add bemyak's layout to bastardkb/scylla

---
+++ keyboards/bastardkb/scylla/keymaps/bemyak/rules.mk
@@ -0,0 +1,5 @@
+MOUSEKEY_ENABLE     = no      # Mouse keys
+NKRO_ENABLE         = no      # USB Nkey Rollover
+RGB_MATRIX_ENABLE   = yes     # Enable keyboard RGB matrix
+RGBLIGHT_ENABLE     = no
+KEY_OVERRIDE_ENABLE = no      # Enable Key Overrides, see https://docs.qmk.fm/#/feature_key_overrides
